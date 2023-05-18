c9ba95da2907d882973c23e697d603c70eeaa329
KingRayhan <rayhan095@gmail.com>
2023-05-18 19:52:55 +0600

    vercel.json

--- src/api/users/dto/user-list-query.dto.ts
+++ src/api/users/dto/user-list-query.dto.ts
@@ -1,5 +1,5 @@
-import { CommonPaginationDto } from '@/shared/dto/CommonPaginationDto';
 import { InputType } from '@nestjs/graphql';
+import { CommonPaginationDto } from '../../../shared/dto/CommonPaginationDto';
 
 @InputType()
 export class UserListQueryDto extends CommonPaginationDto {}
